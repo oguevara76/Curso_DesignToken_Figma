b829b6bd1fe3fc70709d19da60b313062beeafed
Oscar Guevara <94981222+oguevara76@users.noreply.github.com>
2025-03-25 19:52:43 +0000

    Clase-7

--- build.js
+++ build.js
@@ -1,14 +1,44 @@
+import StyleDictionary from 'style-dictionary'
+import { register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from 'sd-themes-loader';
+import { config } from 'process';
+import { platform } from 'os';
+import { transform } from 'typescript';
+import { transforms } from 'style-dictionary/enums';
+
+
+register(StyleDictionary, {
+withSDBuiltins: false,
+
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
 async function run() {
-    // Esto es un comentario
-    const name = "Oscar";
-    const lastname "Guevara";
+    const themes = await loader.load('/tokens');
 
-    const obj = {
-        name = "Hola",
-        age = "48"
-    }
+    const globalThemes = themes.getThemeByName('global');
 
-    console.log(obj.Age);
+    const config = {
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/global/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+
+                transforms: [
+                    'name/kebab',
+                    'ts/resolveMath',
+                    'size/pxToRem'
+                ]
+            }
+        }
+    };
+
+    globalThemes.addConfig(config).build();
 }
 
 run();
