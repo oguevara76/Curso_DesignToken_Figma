b08a289d38af028dd14fd16ba98fc667d5b04fe2
Oscar Guevara <94981222+oguevara76@users.noreply.github.com>
2025-03-31 18:05:00 +0000

    V1.0.1

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import StyleDictionary from 'style-dictionary'
-import { register } from '@tokens-studio/sd-transforms';
+import { expandTypesMap, register } from '@tokens-studio/sd-transforms';
 import ThemesLoader from 'sd-themes-loader';
 import { config } from 'process';
 import { platform } from 'os';
@@ -17,9 +17,18 @@ const loader = ThemesLoader(StyleDictionary);
 async function run() {
     const themes = await loader.load('/tokens');
 
-    const globalThemes = themes.getThemeByName('global');
+    // Cargo los temas específicos a utilizar
+    const globalTheme = themes.getThemeByName('global');
+    const lightTheme = themes.getThemeByName('light');
 
-    const config = {
+
+    const globalConfig = {
+
+        // Esto lo que me genera es que expande las fuentes que están con valores compuestas, heredado desde el Token Studio
+        expand:{
+            typesMap: true,
+        },
+
         platforms: {
             web: {
                 files: [
@@ -32,14 +41,38 @@ async function run() {
                 transforms: [
                     'name/kebab',
                     'ts/resolveMath',
+                    'ts/typography/fontWeight',
+                    'ts/size/lineheight',
                     'size/pxToRem'
                 ]
             }
         }
     };
 
-    
-    globalThemes.addConfig(config).build();
+    const lightConfig = {
+
+        // Desde aqui hago la declaración del fichero para web, iOS, etc y lo creo, más sus transformadores.
+        platforms: {
+            web: {
+                files: [
+                    {   // Creo el fichero CSS y el formato
+                        destination: 'app/build/light/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+
+                transforms: [
+                    // Transformadores de valores para adaptar a la nomenclatura CSS
+                    'name/kebab',
+                    'color/rgb'
+                ]
+
+            }
+        } 
+    }
+
+    globalTheme.addConfig(globalConfig).build();
+    lightTheme.addConfig(lightConfig).build();
 }
 
 run();
