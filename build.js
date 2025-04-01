eeefd63c0cc169c6ec73283881db91f121664c6b
Oscar Guevara <94981222+oguevara76@users.noreply.github.com>
2025-04-01 23:56:01 +0000

    Changes 1ABR25

--- build.js
+++ build.js
@@ -1,15 +1,19 @@
-import StyleDictionary from 'style-dictionary'
+import StyleDictionary from 'style-dictionary';
 import { expandTypesMap, register } from '@tokens-studio/sd-transforms';
 import ThemesLoader from 'sd-themes-loader';
-import { config } from 'process';
-import { platform } from 'os';
-import { transform } from 'typescript';
-import { transforms } from 'style-dictionary/enums';
 
 
 register(StyleDictionary, {
 withSDBuiltins: false,
 
+});
+
+// estas lineas sirven para meter la sintáxis para llamar a la URL del asset. Hemos creado un transformador
+StyleDictionary.registerTransform({
+    name: 'assets/background',
+    type: 'value',
+    filter: (token) => token.$type === 'asset',
+    transform: (token) => `url('/app/assets/${token.$value}')`
 });
 
 const loader = ThemesLoader(StyleDictionary);
@@ -20,6 +24,9 @@ async function run() {
     // Cargo los temas específicos a utilizar
     const globalTheme = themes.getThemeByName('global');
     const lightTheme = themes.getThemeByName('light');
+    const darkTheme = themes.getThemeByName('dark');
+    const desktopTheme = themes.getThemeByName('desktop');
+    const mobileTheme = themes.getThemeByName('mobile');
 
 
     const globalConfig = {
@@ -58,21 +65,109 @@ async function run() {
                     {   // Creo el fichero CSS y el formato
                         destination: 'app/build/light/variables.css',
                         format: 'css/variables',
+                        options: {
+                            selector: '.light'
+                        }
                     }
                 ],
 
                 transforms: [
                     // Transformadores de valores para adaptar a la nomenclatura CSS
                     'name/kebab',
-                    'color/rgb'
+                    'color/rgb',
+                    'assets/background'
                 ]
 
             }
         } 
     }
 
+    const darkConfig = {
+
+        // Desde aqui hago la declaración del fichero para web, iOS, etc y lo creo, más sus transformadores.
+        platforms: {
+            web: {
+                files: [
+                    {   // Creo el fichero CSS y el formato
+                        destination: 'app/build/dark/variables.css',
+                        format: 'css/variables',
+                        options: {
+                            selector: '.dark'
+                        }
+                    }
+                ],
+
+                transforms: [
+                    // Transformadores de valores para adaptar a la nomenclatura CSS
+                    'name/kebab',
+                    'color/rgb',
+                    'assets/background'
+                ]
+
+            }
+        } 
+    }
+
+    const desktopConfig = {
+
+        expand:{
+            typesMap: true,
+        },
+
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/desktop/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+
+                transforms: [
+                    'name/kebab',
+                    'ts/resolveMath',
+                    'ts/typography/fontWeight',
+                    'ts/size/lineheight',
+                    'size/pxToRem'
+                ]
+
+            }
+        } 
+    }
+
+    const mobileConfig = {
+
+        expand:{
+            typesMap: true,
+        },
+
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/mobile/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+
+                transforms: [
+                    'name/kebab',
+                    'ts/resolveMath',
+                    'ts/typography/fontWeight',
+                    'ts/size/lineheight',
+                    'size/pxToRem'
+                ]
+
+            }
+        } 
+    }
+
+    // Ejecuto las instrucciones antriores
     globalTheme.addConfig(globalConfig).build();
     lightTheme.addConfig(lightConfig).build();
+    darkTheme.addConfig(darkConfig).build();
+    desktopTheme.addConfig(desktopConfig).build();
+    mobileTheme.addConfig(mobileConfig).build();
 }
 
 run();
